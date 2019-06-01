64c7c9d09f7cd967fd349ff3c28821b4b246680a
freshchen <961011595@qq.com>
2019-06-01 09:47:30 +0800

    [fresh] update

--- corepython/__init__.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-"""
- @anthor LingChen
- @create 11/29/2018 5:09 PM
- @Description
-"""
--- corepython/cryptology/__init__.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-"""
- @anthor LingChen
- @create 12/4/2018 4:38 PM
- @Description
-"""
